b69a6b5de3fab83e18290e170c27cf691068c1d2
Antonio Cassano <Antonio.Cassano@nhsbsa.nhs.uk>
2023-07-05 14:21:29 +0100

    initial commit

---
+++ src/main/java/net/javaguides/springboorblogwebapp/SpringBoorBlogWebappApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboorblogwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoorBlogWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoorBlogWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/javaguides/springboorblogwebapp/SpringBoorBlogWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboorblogwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoorBlogWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
