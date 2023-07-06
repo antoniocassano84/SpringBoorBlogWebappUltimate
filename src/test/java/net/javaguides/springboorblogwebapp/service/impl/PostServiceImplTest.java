7ee6ffc3ea531bf2032f29534d42eed31177ba0a
Antonio Cassano <Antonio.Cassano@nhsbsa.nhs.uk>
2023-07-06 15:41:52 +0100

    added PostServiceImplTest.java testing for service

--- src/test/java/net/javaguides/springboorblogwebapp/service/impl/PostServiceImplTest.java
+++ src/test/java/net/javaguides/springboorblogwebapp/service/impl/PostServiceImplTest.java
@@ -1,20 +1,33 @@
 package net.javaguides.springboorblogwebapp.service.impl;
 
+import net.javaguides.springboorblogwebapp.mapper.PostMapper;
+import net.javaguides.springboorblogwebapp.repository.PostRepository;
+import net.javaguides.springboorblogwebapp.service.PostService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.verify;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PostServiceImplTest {
+
+    @Mock
+    private PostRepository postRepository;
+    @Mock
+    private PostMapper postMapper;
+    private PostService postService;
 
     @BeforeEach
     void setUp() {
+        postService = new PostServiceImpl(postRepository, postMapper);
     }
 
     @Test
-    void findAllPosts() {
-        fail();
+    void canFindAllPosts() {
+        postService.findAllPosts();
+        verify(postRepository).findAll();
     }
 }
