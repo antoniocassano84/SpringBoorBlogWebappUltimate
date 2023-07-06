675b48e9df7dbdc6877ac54c0b6cf93962382024
Antonio Cassano <Antonio.Cassano@nhsbsa.nhs.uk>
2023-07-06 13:53:03 +0100

    Added PostServiceImplTest.java class

---
+++ src/test/java/net/javaguides/springboorblogwebapp/service/impl/PostServiceImplTest.java
@@ -0,0 +1,20 @@
+package net.javaguides.springboorblogwebapp.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAllPosts() {
+        fail();
+    }
+}
