67fc9e022e2015546a1fc9b9d79b0bd35b10e0b6
Antonio Cassano <Antonio.Cassano@nhsbsa.nhs.uk>
2023-07-06 12:26:42 +0100

    Added PostService.java interface service

---
+++ src/main/java/net/javaguides/springboorblogwebapp/service/PostService.java
@@ -0,0 +1,11 @@
+package net.javaguides.springboorblogwebapp.service;
+
+import net.javaguides.springboorblogwebapp.dto.PostDto;
+
+import java.util.List;
+
+public interface PostService {
+
+    List<PostDto> findAllPosts();
+
+}
