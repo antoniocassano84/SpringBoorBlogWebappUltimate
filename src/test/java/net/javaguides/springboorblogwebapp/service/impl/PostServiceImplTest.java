7ab74390964ab891fb08f9f524bcb2ba93f093a5
Antonio Cassano <Antonio.Cassano@nhsbsa.nhs.uk>
2023-07-12 15:11:44 +0100

    added canFindPaginatedPostsDesc and canFindPaginatedPostsAsc tests in PostServiceImplTest.java

--- src/test/java/net/javaguides/springboorblogwebapp/service/impl/PostServiceImplTest.java
+++ src/test/java/net/javaguides/springboorblogwebapp/service/impl/PostServiceImplTest.java
@@ -11,8 +11,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 
@@ -46,5 +51,31 @@ class PostServiceImplTest {
         ArgumentCaptor<Post> postArgumentCaptor = ArgumentCaptor.forClass(Post.class);
         verify(postRepository).save(postArgumentCaptor.capture());
         assertThat(postArgumentCaptor.getValue()).isEqualTo(postMapper.mapToPost(postDto));
+    }
+
+    @Test
+    void canFindPaginatedPostsDesc() {
+        given(postRepository.findAll(any(PageRequest.class)))
+                .willReturn(new PageImpl<>(List.of(Post.builder().build(), Post.builder().build())));
+
+        postService.findPaginated(1,5, "title", Sort.Direction.DESC.name());
+
+        ArgumentCaptor<Pageable> pageableArgumentCaptor = ArgumentCaptor.forClass(Pageable.class);
+        verify(postRepository).findAll(pageableArgumentCaptor.capture());
+        assertThat(pageableArgumentCaptor.getValue())
+                .isEqualTo(PageRequest.of(1, 5, Sort.by("title").descending()));
+    }
+
+    @Test
+    void canFindPaginatedPostsAsc() {
+        given(postRepository.findAll(any(PageRequest.class)))
+                .willReturn(new PageImpl<>(List.of(Post.builder().build(), Post.builder().build())));
+
+        postService.findPaginated(1,5, "title", Sort.Direction.ASC.name());
+
+        ArgumentCaptor<Pageable> pageableArgumentCaptor = ArgumentCaptor.forClass(Pageable.class);
+        verify(postRepository).findAll(pageableArgumentCaptor.capture());
+        assertThat(pageableArgumentCaptor.getValue())
+                .isEqualTo(PageRequest.of(1, 5, Sort.by("title").ascending()));
     }
 }
