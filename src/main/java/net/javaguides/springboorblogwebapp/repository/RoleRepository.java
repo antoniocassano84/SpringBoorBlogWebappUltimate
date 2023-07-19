de251331cdcefe3e32cc979d51f60a43933f36fa
Antonio Cassano <Antonio.Cassano@nhsbsa.nhs.uk>
2023-07-19 12:16:00 +0100

    added user and role repos

---
+++ src/main/java/net/javaguides/springboorblogwebapp/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package net.javaguides.springboorblogwebapp.repository;
+
+
+import net.javaguides.springboorblogwebapp.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository  extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
